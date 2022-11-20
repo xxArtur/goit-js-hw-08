cadebdefe53f81cb92fe7f37ccb092950b58968a
Artur Solonenko <artur07n73@gmail.com>
2022-11-20 23:05:55 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
